b3089a88f45d72f9c81bb1055b5cd3be160c50a7
Tridev Mishra <Tridev Mishra>
2017-02-15 01:45:49 +0530

    Demonstrated Views & Button ClickListener

--- app/src/main/java/tdevm/tutorial/MainActivity.java
+++ app/src/main/java/tdevm/tutorial/MainActivity.java
@@ -3,59 +3,38 @@ package tdevm.tutorial;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    Button button;
+    EditText editText;
+    TextView textView;
     public static final String TAG = "MERA ACTIVITYYYYYYY";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG,"onCreate was Called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.btn_click_me);
+        editText = (EditText)findViewById(R.id.et_enter_name);
+        textView = (TextView)findViewById(R.id.tv_display_name);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(editText.getText().toString());
+            }
+        });
+
+
     }
 
-    @Override
-    protected void onStart() {
-        Log.d(TAG,"onStart was Called");
-        super.onStart();
-     }
 
-
-    @Override
-    protected void onResume() {
-        Log.d(TAG,"onResume was Called");
-
-        super.onResume();
-    }
-
-
-    @Override
-    protected void onPause() {
-        Log.d(TAG,"onPause was Called");
-
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-
-        Log.d(TAG,"onPause was Called");
-
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-
-        Log.d(TAG,"onDestroy was Called");
-        super.onDestroy();
-    }
-
-    @Override
-    protected void onRestart() {
-
-        Log.d(TAG,"onRestart was Called");
-        super.onRestart();
-    }
 }
